5c450110316f29e10b9d8152e2d8dc32cedb3a0e
Henry Wallis <henrywallis93@gmail.com>
2019-07-30 07:12:12 +0100

    Verify that bets are placed with a valid amount

--- code/bets.py
+++ code/bets.py
@@ -1,8 +1,12 @@
 # TODO
 # - Add more bets
+#   - Pass Line odds
+#   - Come
+# - Put useful info on InvalidBetAmount
+
 
 # Provides the Bet class
-
+from textwrap import dedent
 
 class Bet(object):
     """
@@ -12,9 +16,19 @@ class Bet(object):
 
     def __init__(self, amount):
         self.amount = amount
+        self.bet_type = self.__class__.__name__
+        self._verify_bet()
 
     def __repr__(self):
-        return(self.__class__.__name__ + "({})".format(self.amount))
+        return(self.bet_type + "({})".format(self.amount))
+
+    def _verify_bet(self):
+        if self.amount % 5 != 0:
+
+            explanation = dedent("""
+            Amount must be a multiple of 5
+            """)
+            raise InvalidBetAmount(self.bet_type, self.amount, explanation)
 
     def get_winnings(self, roll, point):
         pass
@@ -46,3 +60,32 @@ class PassLineBet(Bet):
         else:
             return 0
 
+
+class PassLineOdds(Bet):
+    """
+    Odds behind the Pass Line bet
+    - Not working if the point is off
+    - Pays out when the point is hit:
+      - Point is 4 or 10 => Pays 2-to-1
+      - Point is 5 or 9 => Pays 3-to-2
+      - Point is 6 or 8 => Pays 6-to-5
+    - Loses on 7-out
+    """
+    pass
+
+
+class InvalidBetAmount(Exception):
+    """
+    Raise when attempting to create a bet with a disallowed amount
+    """
+    def __init__(self, bet, amount, info):
+        self.bet = bet
+        self.amount = amount
+        self.info = info
+
+    def __str__(self):
+        return(dedent("""
+            Attempted to place a bet with an invalid amount:
+            Bet type - {bet}
+            Amount - {amount} {info}""").format(
+            bet=self.bet,amount=self.amount,info=self.info))
